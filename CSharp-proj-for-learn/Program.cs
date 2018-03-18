b3ff191828dcfaea9be83205f5089484fa601286
Oleksiy Nekrasov <n.507592979@gmail.com>
2018-03-18 18:26:57 +0100

    s

---
+++ CSharp-proj-for-learn/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CSharp_proj_for_learn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ShowIntro(30);
+        }
+
+        static void ShowIntro(int countSign)
+        {
+            Console.Clear();
+            for (int i= 0; i <= countSign; i++)
+            {
+                Console.Write("*");
+                Thread.Sleep(50);
+            }
+            Console.WriteLine("");
+            Console.WriteLine(".NET and C# project for learn");
+            Console.WriteLine("");
+            for (int i = 0; i <= countSign; i++)
+            {
+                Console.Write("*");
+                Thread.Sleep(100);
+            }
+
+        }
+    }
+}
