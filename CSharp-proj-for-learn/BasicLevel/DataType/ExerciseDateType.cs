b409117f9893c4d4f5f627444e6aa3b8dbff0e79
Oleksiy Nekrasov <n.507592979@gmail.com>
2018-04-04 17:48:17 +0200

    type

---
+++ CSharp-proj-for-learn/BasicLevel/DataType/ExerciseDateType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_proj_for_learn.BasicLevel.DataType
+{
+    class ExerciseDateType
+    {
+        //zrealizowane w https://github.com/oleksiy5/temperature_controller
+        //
+    }
+}
